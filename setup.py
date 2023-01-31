20c4e49a225f0966b08623c518dfcdca82c0dfab
Daniel Zubov <109290535+DanielDDZ@users.noreply.github.com>
2023-01-31 16:57:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(name='clean',
       version='2.0',
-      description='Sorts files in a folder',
+      description='Sorts files in a folder with threads and processes',
       url='https://github.com/DanielDDZ/clean_folder.2.0',
       author='Daniel Zubov',
       author_email='danielzubov12@gmail.com',
